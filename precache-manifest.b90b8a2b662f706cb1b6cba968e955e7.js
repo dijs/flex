5270f310f95f63209f934a7559974846839bf142
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-27 12:38:40 +0000

    Deploy dijs/flex to github.com/dijs/flex.git:gh-pages

---
+++ precache-manifest.b90b8a2b662f706cb1b6cba968e955e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245a04c4847c0a1933dee2d0519a09c3",
+    "url": "/flex/index.html"
+  },
+  {
+    "revision": "0b1cd2e86492cbb1ded6",
+    "url": "/flex/static/css/main.a762f1de.chunk.css"
+  },
+  {
+    "revision": "7bf0b45162b980131158",
+    "url": "/flex/static/js/2.5ca32c90.chunk.js"
+  },
+  {
+    "revision": "0b1cd2e86492cbb1ded6",
+    "url": "/flex/static/js/main.64610696.chunk.js"
+  },
+  {
+    "revision": "1c038550a4fb41558193",
+    "url": "/flex/static/js/runtime~main.a4e69e30.js"
+  },
+  {
+    "revision": "49cb8bef3f70f933371ca10c54037d58",
+    "url": "/flex/static/media/logo.49cb8bef.svg"
+  }
+]);
